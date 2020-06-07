dc3500ddb2adc56d599a7d305bf07d2259ca338f
Joel Linn <jl@conductive.de>
2020-06-08 00:10:51 +0200

    Working example.

---
+++ main_works.cpp
@@ -0,0 +1,60 @@
+﻿#include <cassert>
+#include <iostream>
+
+#include <Windows.h>
+
+HANDLE mutex;
+
+HANDLE thread_a;
+HANDLE thread_b;
+
+// thread main
+DWORD WINAPI main_spin(LPCSTR ident) {
+    int i = 0;
+    while (i < 10) {
+        {
+            assert(WaitForSingleObject(mutex, INFINITE) == WAIT_OBJECT_0);
+            std::cout << ident << " spinning..  " << ++i << std::endl;
+            ReleaseMutex(mutex);
+        }
+        Sleep(100);
+    }
+    return 0;
+}
+
+int main()
+{
+    SetThreadDescription(GetCurrentThread(), L"main");
+
+    mutex = CreateMutexA(NULL, FALSE, "global_critical_region");
+    assert(mutex);
+
+    thread_a = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)main_spin, "A" , 0, NULL);
+    assert(thread_a);
+    SetThreadDescription(thread_a, L"main_spin_A");
+
+    thread_b = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)main_spin, "B", 0, NULL);
+    assert(thread_b);
+    SetThreadDescription(thread_b, L"main_spin_B");
+
+    Sleep(500);
+
+    {
+        assert(WaitForSingleObject(mutex, INFINITE) == WAIT_OBJECT_0);
+        std::cout << "I'm in danger..." << std::endl;
+        Sleep(500);
+        // Suspend thread 'a' which is trying to lock the mutex_
+        SuspendThread(thread_a);
+        ReleaseMutex(mutex);
+    }
+
+    WaitForSingleObject(thread_b, INFINITE);
+    TerminateThread(thread_a, 0);
+
+    CloseHandle(thread_a);
+    CloseHandle(thread_b);
+
+    CloseHandle(mutex);
+
+    return 0;
+}
